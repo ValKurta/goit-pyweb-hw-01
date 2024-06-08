6931cc4180e08dd7096aa54630a8c85b2293857b
ValKurta <valentyn.kurta@gmail.com>
2024-06-08 19:52:00 +0200

    2.0:modified

--- contact_manager/main.py
+++ contact_manager/main.py
@@ -1,6 +1,7 @@
+import pickle
 from addressbook import AddressBook
 from view import ConsoleView
-from utils import parse_input, load_data
+from utils import parse_input, load_data, file_path
 from commands import (
     add_contact,
     change_contact,
@@ -24,28 +25,25 @@ def main():
             with open(file_path, "wb") as file:
                 pickle.dump(book, file)
             break
-
         elif command == "hello":
             view.display_message("How can I help you?")
-
         elif command == "add":
             view.display_message(add_contact(args, book))
-
         elif command == "change":
             view.display_message(change_contact(args, book))
-
         elif command == "phone":
             view.display_message(show_phone(args, book))
-
         elif command == "all":
             view.display_contacts(show_all(book))
-
         elif command == "add-birthday":
             view.display_message(add_birthday(args, book))
-
         elif command == "show-birthday":
             view.display_message(show_birthday(args, book))
-
         elif command == "birthdays":
             view.display_birthdays(book.get_upcoming_birthdays())
+        else:
+            view.display_message("Invalid command.")
 
+
+if __name__ == "__main__":
+    main()
